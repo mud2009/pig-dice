b7a57bc314623f12fa698dd8be23ec997dffd472
Will Greenberg <greenberg.will@gmail.com>
2022-01-26 11:12:15 -0800

    add functions for roll, turntally,total score
    Co-authored-by: Jessica B <jessb-epicodus@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+Describe : roll()
+
+Test: We expect this function to return a random number from 1 to 6
+Code:
+roll()
+Expected outcome: 4
---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+// PigDice logic
+function PigDice () {
+  this.players = {};
+  this.currentId = 0;
+}
+
+PigDice.prototype.addPlayer = function (player) {
+  player.id = this.assignId();
+  this.players[player.id] = player;
+};
+
+PigDice.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+PigDice.prototype.newRoll = function(id) {
+  this.players[id].roll = 0;
+  this.players[id].roll = roll();
+};
+
+PigDice.prototype.addRollToTurnTally = function(id) {
+  this.players[id].turnTally = 0;
+  this.players[id].turnTally = this.players[id].turnTally + this.players[id].roll;
+};
+
+PigDice.prototype.addTurnTallyToTotal = function(id) {
+  this.players[id].totalScore = this.players[id].totalScore + this.players[id].turnTally
+}
+
+PigDice.prototype
+
+// Player logic
+
+function Player() {
+  this.roll = 0;
+  this.turnTally = 0;
+  this.totalScore = 0;
+}
+
+function roll(){
+  return Math.floor((Math.random()*6) +1);
+};
+
+// UI logic
+let pigDice = new PigDice();
+let player1 = new Player(1);
+let player2 = new Player(2);
+pigDice.addPlayer(player1);
+pigDice.addPlayer(player2);
